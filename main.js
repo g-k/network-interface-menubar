25bc03576cfdc0fe834552ac45bda2a27bd6d24a
Greg Guthe <greg.guthe@gmail.com>
2015-08-04 22:28:46 -0400

    Add basic menubar display

---
+++ main.js
@@ -0,0 +1,42 @@
+var path = require('path');
+
+var menubar = require('menubar');
+var network = require('network');
+
+var icons = {
+  unknown: path.join(__dirname, 'network-wired-acquiring.@2.5x.png'),
+  disconnected: path.join(__dirname, 'network-wired-disconnected.@2.5x.png'),
+  wired: path.join(__dirname, 'network-wired.@2.5x.png'),
+};
+
+var mb = menubar({
+  dir: __dirname,
+  icon: icons.unknown,
+  width: 350,
+  height: 200,
+});
+
+var pollFrequency = 3000; // ms between checks
+
+var updateIcon = function () {
+  network.get_active_interface(function (e, r) {
+    // console.debug(e, r);
+
+    if (r && r.type && r.type === 'Wired') {
+      mb.tray.setImage(icons.wired);
+    } else {
+      mb.tray.setImage(icons.disconnected);
+    };
+
+    if (mb.window && mb.window.webContents) {
+      mb.window.webContents.send('default-network-state', JSON.stringify(r));
+    }
+
+    // TODO: backoff if the state hasn't changed for awhile
+    setTimeout(updateIcon, pollFrequency);
+  });
+};
+
+mb.on('ready', function ready () {
+  setTimeout(updateIcon, pollFrequency);
+});
